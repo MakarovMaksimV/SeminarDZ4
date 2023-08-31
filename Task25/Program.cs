aa65b705ac35836f3047dc8cc2eb8e269afa0ec8
Maksim <mirror0791@mail.ru>
2023-08-31 22:24:20 +0300

    Task25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,3 +1,35 @@
 ﻿/*Задача 25. Напишите цикл, который принимает на вход два числа (А и В)
 и возводит А в натуральную степень числа В*/
+using System;
 
+namespace Task25
+{
+    class Program
+    {
+        static void Main()
+        {   
+            PrintBase();  
+            double numberA = Convert.ToDouble(Console.ReadLine());
+            PrintExponentiation();
+            double numberB = Convert.ToDouble(Console.ReadLine());
+            
+            RaiseToThePower(numberA, numberB);
+        }
+
+        public static void RaiseToThePower (double numberX, double numberY)
+        {
+            double result = Math.Pow(numberX,numberY);
+            Console.WriteLine("Результат возведения в степень: " + result);
+        }
+
+        public static void PrintBase()
+        {
+            Console.Write("Введите число основание: ");
+        }
+        public static void PrintExponentiation()
+        {
+            Console.Write("Введите число множитель: ");
+        }       
+    }
+
+}
