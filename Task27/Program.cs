94a27105afeb8a42c4cb33116c23f5c7c0f4294d
Maksim <mirror0791@mail.ru>
2023-09-01 00:13:39 +0300

    Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Task27
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int summResult = Summ(number);
+            System.Console.WriteLine("Результат сложения всех цифр в числе: " + summResult);
+        }
+
+        public static int Summ(int number)
+        {
+            int result  = 0;
+            while(number > 0)
+            {
+                result += number % 10;
+                number = number / 10; 
+            }
+            return result;
+        }
+    }
+}    
